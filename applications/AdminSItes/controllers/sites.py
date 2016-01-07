d034c90fb3b3b13184ead89c0e7429e01056e464
Raphael Iarussi <pigaov10@gmail.com>
2016-01-07 09:54:28 -0200

    dir profiles dentro do projeto

--- applications/AdminSItes/controllers/sites.py
+++ applications/AdminSItes/controllers/sites.py
@@ -1,20 +1,45 @@
 ### SITE PLONE
+import os, sys
 @auth.requires_membership('admin')
 def add():
-	import os, sys
+	"""
+	MÉTODO RESPONSÁVEL POR GERAR O SKELETON DE UM SITE PLONE
+	@dir /sites/prodam.gerenciador.<project_name>/src/prodam/gerenciador/<project_name>/
+	"""
+
 	if request.post_vars.site:
 		site = request.post_vars.site
-		path = str("sites/prodam.gerenciador.")+str(site)
+		folders = ['sites','/viewlets','/browser','/src','/templates']
 		try:
-			os.mkdir( path, 0755 );
-			path = str("sites/prodam.gerenciador.")+str(site)+str('/src')
-			os.mkdir( path, 0755 );
-			path = str("sites/prodam.gerenciador.")+str(site)+str('/src/prodam')
-			os.mkdir( path, 0755 );
-			path = str("sites/prodam.gerenciador.")+str(site)+str('/src/prodam/gerenciador')
-			os.mkdir( path, 0755 );
-			path = str("sites/prodam.gerenciador.")+str(site)+str('/src/prodam/gerenciador/')+str(site)
-			os.mkdir( path, 0755 );
+			project_name = "prodam.gerenciador."+str(site)
+			path   = folders[0]
+			path  += "/"+str(project_name)
+			os.mkdir(path, 0755 ); # plone name site
+			path  += folders[3]
+			os.mkdir(path, 0755 ); # directory src
+
+			directories = project_name.split(".")
+			for directory in directories:
+				path += "/"+directory
+				os.mkdir(path, 0755 );
+			# browser
+			profiles = path
+			os.mkdir(profiles+"/profiles", 0755);
+
+			path += folders[2]
+			os.mkdir(path, 0755);
+			# viewlets
+			path += folders[1]
+			os.mkdir(path, 0755);
+			# zcml file
+			add_configure_file(path)
+			# python file
+			add_viewlets_file(path)
+			# templates
+			path += folders[4]
+			os.mkdir(path, 0755);
+			# pt file 
+			add_template_file(path)
 		except:
 			raise HTTP(500, T('Erro ao criar o diretório'))
 	else:
@@ -22,5 +47,48 @@ def add():
 	lista = os.listdir("sites/")
 	return dict(lista=lista,site=site)
 
+def add_configure_file(path):
+	"""
+	MÉTODO RESPONSÁVEL POR GERAR O ARQUIVO ZCML 
+	"""
 
+	file_name = "configure.zcml"
+	file = open(path+"/"+file_name,"a+")
+	text = """<configure
+    xmlns="http://namespaces.zope.org/zope"
+    xmlns:browser="http://namespaces.zope.org/browser"
+    i18n_domain="prodam.portal">
 
+  <browser:viewlet
+      name="prodam.prefeitura.alertas"
+      manager="plone.app.layout.viewlets.interfaces.IPortalHeader"
+      class=".viewlets.DefaultViewlet"
+      layer="prodam.portal.interfaces.IProdamPortal"
+      template="templates/alerta.pt"
+      permission="zope2.View"
+      />
+</configure>
+			"""
+	file.write(text)
+	file.close()
+	return True
+
+def add_template_file(path):
+	"""
+	MÉTODO RESPONSÁVEL POR GERAR O TEMPLATE alerta.pt 
+	"""
+
+	file_name = "alerta.pt"
+	file = open(path+"/"+file_name,"a+")
+	file.close()
+	return True
+
+def add_viewlets_file(path):
+	"""
+	MÉTODO RESPONSÁVEL POR GERAR O viewlets.py 
+	"""
+
+	file_name = "viewlets.py"
+	file = open(path+"/"+file_name,"a+")
+	file.close()
+	return True
